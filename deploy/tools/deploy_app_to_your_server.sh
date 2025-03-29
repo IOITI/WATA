64bc33a0f6c73abb28749d2f39704b9765ad10bb
ioiti <22798250+IOITI@users.noreply.github.com>
2025-03-29 16:56:58 +0100

    Update deployment scripts and inventory file path

---
+++ deploy/tools/deploy_app_to_your_server.sh
@@ -0,0 +1,15 @@
+# Read the version from the VERSION file
+VERSION=$(cat ../../VERSION)
+
+# Check if the inventory file exists
+INVENTORY_FILE="ansible/inventory/inventory.ini"
+if [ ! -f "$INVENTORY_FILE" ]; then
+  echo "Error: Inventory file $INVENTORY_FILE does not exist, please create it, in example from 'deploy/tools/ansible/inventory/inventory_example.ini'"
+  exit 1
+fi
+
+echo "Send WATA version $VERSION to server"
+
+ansible-playbook ansible/playbook/deploy_app.yml -i "$INVENTORY_FILE"
+
+echo "WATA version $VERSION can be started on the server"
--- deploy/tools/send_to_vps.sh
+++
@@ -1,8 +0,0 @@
-# Read the version from the VERSION file
-VERSION=$(cat ../../VERSION)
-
-echo "Send WATA version $VERSION to server"
-
-ansible-playbook deploy_app.yml -i inventory.ini
-
-echo "WATA version $VERSION can be started on the server"
